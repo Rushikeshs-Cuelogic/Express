f4cb19c055f1374a07acdc66fda04ccee159bf5b
cuelogic@123 <rushikesh.shelar@cuelogic.com>
2017-05-15 12:05:19 +0530

    changes for call api

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 
 var routes = require('./routes');
-
 app.get('/', routes.index);
+
+routes.requests = require('./routes/request').request;
+app.get('/request', routes.requests);
 
 app.listen(8000);
